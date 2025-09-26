2b074a5037f71ff58586205cf57ed8999222fc5e
Riham274 <riham.ararawi274@gmail.com>
2025-09-27 00:15:33 +0300

    Add TodoList component to render TodoItem list with delete functionality

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,17 @@
+import TodoItem from "./TodoITem";
+
+function TodoList({ items, deleteItem }) {
+  return (
+    <ul className="space-y-2">
+      {items.map((item, index) => {
+        <TodoItem
+          key={index}
+          item={item}
+          deleteItem={() => deleteItem(index)}
+        />;
+      })}
+    </ul>
+  );
+}
+
+export default TodoList;
