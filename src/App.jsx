3f295b0d40cd9d0f0b5720c48e321e8adc58178a
Riham274 <riham.ararawi274@gmail.com>
2025-09-27 00:28:57 +0300

    Setup App component with TodoForm, TodoList, add and delete functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
-import "./App.css";
+import { useState } from "react";
 import TodoForm from "./components/AddForm";
 import TodoItem from "./components/TodoITem";
+import TodoList from "./components/TodoList";
 
 function App() {
+  const [items, setItems] = useState([
+    "JavaScript",
+    "HTML",
+    "CSS",
+    "React",
+    "Angular",
+    "NextJS",
+  ]);
+  const [newItem, setNewItem] = useState("");
+
+  const addItem = (e) => {
+    e.preventDefault();
+    setItems([...items, newItem]);
+  };
+
+  const deleteItem = (index) => {
+    const newItems = [...items];
+    newItems.splice(index, 1);
+    setItems(newItems);
+  };
   return (
-    <>
-      <TodoForm />
-      <TodoItem />
-    </>
+    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+      <div className="bg-white shadow p-6 rounded-2xl w-full max-w-md">
+        <h1 className="text-center text-2xl font-bold mb-4">To Do List</h1>
+
+        <TodoForm newItem={newItem} setNewItem={setNewItem} addItem={addItem} />
+
+        <TodoList items={items} deleteItem={deleteItem} />
+      </div>
+    </div>
   );
 }
 
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,13 +3,13 @@ import TodoItem from "./TodoITem";
 function TodoList({ items, deleteItem }) {
   return (
     <ul className="space-y-2">
-      {items.map((item, index) => {
+      {items.map((item, index) => (
         <TodoItem
           key={index}
           item={item}
           deleteItem={() => deleteItem(index)}
-        />;
-      })}
+        />
+      ))}
     </ul>
   );
 }
